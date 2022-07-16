6e645868f0c8bd44171b60b876a86319911fec02
Sarvmbh-Sawant <sarvmbh742@gmail.com>
2022-07-16 16:02:16 +0530

    arithmetic operator

---
+++ arithmetic operator.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a , b;
+	cout<<"a:";
+	cin>>a;
+	cout<<"b:";
+	cin>>b;
+	
+	
+	cout<<"a+b = "<<a+b<<endl;
+	
+	cout <<"a-b = "<<a-b<<endl;
+	
+	cout<<"a*b ="<<a*b<<endl;
+	
+	cout<<"a/b ="<<a/b<<endl;
+	
+	cout<<"a%b ="<<a%b<<endl;
+	
+	return 0;
+}
